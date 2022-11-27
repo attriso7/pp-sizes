f8a638f7c7e90e11deab06ebc85191341e95dcae
attriso7 <46514732+attriso7@users.noreply.github.com>
2022-11-27 09:43:08 +0530

    Add files via upload

---
+++ pp size.py
@@ -0,0 +1,26 @@
+
+
+siz = int(input("What's your pp size!!\nshh it will be a secret \n\n>> "))
+sizee = f'{siz} inches'
+size = sizee
+if siz == 1:
+    print(f' {sizee},!! You a fuckin child or what mate?')
+elif siz == 2:
+    print(f" {sizee},!! That didn't quite grew.. did it?")
+elif siz == 3:
+    print(f" {sizee},!! You are barely there young man")
+elif siz == 4:
+    print(f"{sizee},!! erected or without erection?")
+elif siz == 5:
+    print(f"{sizee},!! Ahh she starts to feel it..")
+elif siz == 6:
+    print(f"{sizee},!! uhh average")
+elif siz == 7:
+    print(f"{sizee},!! that's the golden number ma man")
+elif siz == 8:
+    print(f"{sizee},!! IT's started hurting.. Ahh..")
+elif siz == 9:
+    print(f"{sizee},!! You monster")
+else : print("That's out of my scale")
+
+quit()
