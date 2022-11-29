cc045b55afcfb7b01c5463c59c17c5802f1be777
attriso7 <tbagde50@gmail.com>
2022-11-30 02:12:29 +0530

    made it a bit better

--- pp size.py
+++ pp size.py
@@ -1,6 +1,33 @@
-# i was just bored and wanted to practice the if-else-elif statement i just learned
-# so i made this fun line of code for this
+# I was just bored and wanted to practice the if-else-elif statement I just learned
+# so I made this fun line of code for this
 # do get offended if you must as this are my views!!
+
+def ppsize():
+    if siz < 1:
+        return "You kidding me right?\n Right!!"
+    elif siz < 2:
+        return f' {sizee},!! You a fuckin child or what mate?'
+    elif siz < 3:
+        return f" {sizee},!! That didn't quite grew.. did it?"
+    elif siz < 4:
+        return f" {sizee},!! You are barely there young man"
+    elif siz < 5:
+        return f"{sizee},!! erected or without erection?"
+    elif siz < 6:
+        return f"{sizee},!! Ahh she starts to feel it.."
+    elif siz < 7:
+        return f"{sizee},!! uhh average"
+    elif siz < 8:
+        return f"{sizee},!! that's the golden number ma man"
+    elif siz < 9:
+        return f"{sizee},!! IT's started hurting.. Ahh.."
+    elif siz < 10:
+        return f"{sizee},!! You monster"
+
+    else:
+        return "That's out of my scale"
+
+
 try:
     siz = float(input("What's your pp size!!\nshh it will be a secret \n\n>> "))
     sizee = f'{siz} inches'
@@ -9,27 +36,6 @@ except:
     print("Please put a numeric value.. Bitch!!\n have some fuckin sence")
     quit()
 
-if siz < 1:
-    print("You kidding me right?\n Right!!")
-elif siz < 2:
-    print(f' {sizee},!! You a fuckin child or what mate?')
-elif siz < 3:
-    print(f" {sizee},!! That didn't quite grew.. did it?")
-elif siz < 4:
-    print(f" {sizee},!! You are barely there young man")
-elif siz < 5:
-    print(f"{sizee},!! erected or without erection?")
-elif siz < 6:
-    print(f"{sizee},!! Ahh she starts to feel it..")
-elif siz < 7:
-    print(f"{sizee},!! uhh average")
-elif siz < 8:
-    print(f"{sizee},!! that's the golden number ma man")
-elif siz < 9:
-    print(f"{sizee},!! IT's started hurting.. Ahh..")
-elif siz < 10:
-    print(f"{sizee},!! You monster")
-
-else : print("That's out of my scale")
+print(ppsize())
 
 quit()
